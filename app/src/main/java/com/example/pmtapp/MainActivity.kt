97c1a56d6b22c281b2b981ca94edf1d4881bd7f7
AdamBaish <91488420+AdamBaish@users.noreply.github.com>
2022-05-12 19:13:30 +0100

    Created 1st iteration of page to change the font size and type

--- app/src/main/java/com/example/pmtapp/MainActivity.kt
+++ app/src/main/java/com/example/pmtapp/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.pmtapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val text_button: Button = findViewById<Button>(R.id.text_button)
+        text_button.setOnClickListener {
+            val intent = Intent(this, TextActivity::class.java).apply{}
+            ContextCompat.startActivity(this, intent, null)
+        }
     }
 }
