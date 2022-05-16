a97eba40c783fc4411d8023b08838c24e61678cc
AdamBaish <91488420+AdamBaish@users.noreply.github.com>
2022-05-16 20:15:21 +0100

    Moved function to correct file

--- app/src/main/java/com/example/pmtapp/UserSelectionActivity.kt
+++ app/src/main/java/com/example/pmtapp/UserSelectionActivity.kt
@@ -9,25 +9,5 @@ class UserSelectionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_selection)
 
-
-
-
     }
-
-    /*
-    this function creates a new text file and saves the username, age and gender into it
-    note: if the file is already there, this function will overwrite it. This will only happen if the user enters a name
-    that is already taken
-     */
-    fun createNewProfile(userName: String, age: Int, gender: String) {
-
-        val fileName = "$userName.txt"
-
-        var file = File(fileName)
-
-        // create a new file
-        file.writeText("$userName,$age,$gender")
-
-    }
-
 }
