6d7fe010892bff9f5f38df32f9e7881e8620f752
AdamBaish <91488420+AdamBaish@users.noreply.github.com>
2022-05-16 20:13:54 +0100

    Added a function to create new user profiles and save them to a text file

--- app/src/main/java/com/example/pmtapp/UserSelectionActivity.kt
+++ app/src/main/java/com/example/pmtapp/UserSelectionActivity.kt
@@ -2,10 +2,32 @@ package com.example.pmtapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.io.File
 
 class UserSelectionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_selection)
+
+
+
+
     }
+
+    /*
+    this function creates a new text file and saves the username, age and gender into it
+    note: if the file is already there, this function will overwrite it. This will only happen if the user enters a name
+    that is already taken
+     */
+    fun createNewProfile(userName: String, age: Int, gender: String) {
+
+        val fileName = "$userName.txt"
+
+        var file = File(fileName)
+
+        // create a new file
+        file.writeText("$userName,$age,$gender")
+
+    }
+
 }
