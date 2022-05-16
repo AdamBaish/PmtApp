e79c326c2b4a1ae49eb51ba62c8cf554b924bbe6
AdamBaish <91488420+AdamBaish@users.noreply.github.com>
2022-05-16 20:50:24 +0100

    added try and catch to avoid crashing when the username is already taken

--- app/src/main/java/com/example/pmtapp/UserCreation.kt
+++ app/src/main/java/com/example/pmtapp/UserCreation.kt
@@ -1,13 +1,17 @@
 package com.example.pmtapp
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStreamWriter
 
 class UserCreation : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,14 +34,16 @@ class UserCreation : AppCompatActivity() {
             var age = ageString.toInt()
 
             //create the new profile by calling the function
-            createNewProfile(userName,age,gender)
-
-            //pass back to user selection
-            val intent = Intent(this, UserSelectionActivity::class.java).apply{}
-            ContextCompat.startActivity(this, intent, null)
+            var success = createNewProfile(userName,age,gender)
+            if (success){
+                //pass back to user selection
+                val intent = Intent(this, UserSelectionActivity::class.java).apply{}
+                ContextCompat.startActivity(this, intent, null)
+            }else{
+                //inform the user there has been an error
+                Toast.makeText(baseContext, "error, please try again with a new user name", Toast.LENGTH_SHORT).show()
+            }
         }
-
-
     }
 
 
@@ -46,14 +52,20 @@ class UserCreation : AppCompatActivity() {
     note: if the file is already there, this function will overwrite it. This will only happen if the user enters a name
     that is already taken
      */
-    private fun createNewProfile(userName: String, age: Int, gender: String) {
-
-        val fileName = "$userName.txt"
-
-        var file = File(fileName)
-
-        // create a new file
-        file.writeText("$userName,$age,$gender")
+    private fun createNewProfile(userName: String, age: Int, gender: String): Boolean {
+        try {
+            val fileOutputStream: FileOutputStream = openFileOutput("$userName.txt", Context.MODE_PRIVATE)
+            val outputWriter = OutputStreamWriter(fileOutputStream)
+            outputWriter.write("$userName,$age,$gender")
+            outputWriter.close()
+            //display file saved message
+            Toast.makeText(baseContext, "File saved successfully!", Toast.LENGTH_SHORT).show()
+            return true
+        }
+        catch (e: Exception) {
+            e.printStackTrace()
+            return false
+        }
 
     }
 }
