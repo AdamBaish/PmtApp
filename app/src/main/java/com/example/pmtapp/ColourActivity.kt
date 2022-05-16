951aafecc404dc0ccbec4eb938302672586fa8e2
Max Conradi <s5309711@bournemouth.ac.uk.com>
2022-05-16 23:43:18 +0100

    Initial Commit

--- app/src/main/java/com/example/pmtapp/ColourActivity.kt
+++ app/src/main/java/com/example/pmtapp/ColourActivity.kt
@@ -1,10 +1,16 @@
 package com.example.pmtapp
 
 import android.content.Intent
+import android.graphics.PorterDuff
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.core.content.ContextCompat
+import org.w3c.dom.Document
+import javax.xml.parsers.DocumentBuilder
+import javax.xml.parsers.DocumentBuilderFactory
 
 class ColourActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +21,12 @@ class ColourActivity : AppCompatActivity() {
         backButtonColourPage.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java).apply{}
             ContextCompat.startActivity(this, intent, null)
+        }
+
+        val redGreenColourBlind: Button = findViewById<Button>(R.id.RGbutton)
+        val imageView: ImageView = findViewById<ImageView>(R.id.imageView)
+        redGreenColourBlind.setOnClickListener {
+            imageView.setColorFilter(0x76ffffff, PorterDuff.Mode.MULTIPLY );
         }
     }
 }
